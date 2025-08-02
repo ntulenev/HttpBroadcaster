63875bab629353451b892e1962a0bffb27ec9173
Nikita Tulenev <n1tulenev@gmail.com>
2025-08-02 20:59:32 +0200

    Removes environment-specific outbox writer retrieval
    Removes the `GetWriter` method from the `IUnitOfWork` interface.
    
    This simplifies the interface and removes the need to retrieve an outbox writer based on the environment, promoting a more streamlined approach to managing outbox writers.

--- src/Abstractions/IUnitOfWork.cs
+++ src/Abstractions/IUnitOfWork.cs
@@ -12,13 +12,6 @@ public interface IUnitOfWork : IAsyncDisposable
     IReadOnlyCollection<IOutboxWriter> Writers { get; }
 
     /// <summary>
-    /// Retrieves the outbox writer for the specified environment.
-    /// </summary>
-    /// <param name="environment">The target environment.</param>
-    /// <returns>The associated <see cref="IOutboxWriter"/> instance.</returns>
-    IOutboxWriter GetWriter(Models.Environment environment);
-
-    /// <summary>
     /// Saves all pending changes and commits the transaction.
     /// </summary>
     /// <param name="cancellationToken">A token to observe while waiting for the task to complete.</param>
