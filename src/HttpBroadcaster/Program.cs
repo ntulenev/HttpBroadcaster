788fd1dba569e11b816514b60b0ddc569201a2e0
Nikita Tulenev <n1tulenev@gmail.com>
2025-08-02 18:47:12 +0200

    Initializes .NET 9 project structure
    Sets up a new .NET 9 solution with multiple projects.
    
    Includes core project files, configurations, and enables strict code analysis.
    
    Adds an .editorconfig file to define coding style rules, formatting, and naming conventions for the project, ensuring consistency across the codebase.

---
+++ src/HttpBroadcaster/Program.cs
@@ -0,0 +1,43 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    _ = app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+#pragma warning disable CA5394 // Do not use insecure randomness
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+#pragma warning restore CA5394 // Do not use insecure randomness
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+internal sealed record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
