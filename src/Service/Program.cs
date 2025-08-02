84c3bf797f5941f06f2a48897747dde05b2fdd3b
Nikita Tulenev <n1tulenev@gmail.com>
2025-08-02 20:17:50 +0200

    Implements basic messaging endpoint
    Sets up a basic messaging endpoint with health checks and OpenAPI support.
    
    Removes the weather forecast endpoint.

--- src/Service/Program.cs
+++ src/Service/Program.cs
@@ -1,12 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     _ = app.MapOpenApi();
@@ -14,30 +12,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapHealthChecks("/hc");
 
-app.MapGet("/weatherforecast", () =>
+app.MapPost("api/messages", async (
+    Transport.InboundMessage message,
+    CancellationToken ct) =>
 {
-#pragma warning disable CA5394 // Do not use insecure randomness
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-#pragma warning restore CA5394 // Do not use insecure randomness
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    await Task.Delay(100, ct);
+    return Results.Ok();
+});
 
 app.Run();
-
-internal sealed record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
